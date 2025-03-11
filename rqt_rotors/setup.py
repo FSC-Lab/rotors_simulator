b02f622dcd083dfe97fc61e167b78675a9183605
H S Helson Go <hellston20a@gmail.com>
2025-03-11 16:28:02 -0400

    Clean: Remove hil

--- rqt_rotors/setup.py
+++ rqt_rotors/setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['rqt_rotors'],
-    package_dir={'': 'src'},
-    scripts=['scripts/hil_plugin']
+    packages=["rqt_rotors"], package_dir={"": "src"}, scripts=[]
 )
 
 setup(**setup_args)
